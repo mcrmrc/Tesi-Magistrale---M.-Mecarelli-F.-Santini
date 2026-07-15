14b0ff06ee73fe98cf047e16e364d5d56371b543
Marco Mecarelli <61580913+mcrmrc@users.noreply.github.com>
2026-07-15 21:19:02 +0200

    modified classes

--- Programma/classes/Get-Attack-Function.py
+++ Programma/classes/Get-Attack-Function.py
@@ -1,24 +1,40 @@
+<<<<<<< HEAD
 import json
 import time
 import os 
 import json
+=======
+import time
+>>>>>>> d122fbe (modified classes)
 from enum import Enum
 from Programma.custom_enum import ATTACK_TYPE
 from Programma.methods.check_type import is_enum_member, is_integer, is_string
 from Programma.methods.utils_methods import ask_bool_choice
+<<<<<<< HEAD
 from ..custom_enum import print_enum
+=======
+>>>>>>> d122fbe (modified classes)
 
 
 class GET_ATTACK_FUNCTION: 
     def choose_attack_function(): 
         attack_enum=None
         while True: 
+<<<<<<< HEAD
             print(GET_ATTACK_FUNCTION.print_available_attack(),"\n")
             msg="Scegli il nome o il codice della funzione:\t" 
             scelta=str(input(msg)).lower().strip() 
             print("Hai scelto: ",scelta if str(scelta)!="" else "<empty>") 
             attack_enum=GET_ATTACK_FUNCTION.get_attack_method(scelta) 
             if is_enum_member(attack_enum,GET_ATTACK_FUNCTION): 
+=======
+            print(ATTACK_TYPE.print_available_attack(),"\n")
+            msg="Scegli il nome o il codice della funzione:\t" 
+            scelta=str(input(msg)).lower().strip() 
+            print("Hai scelto: ",scelta if str(scelta)!="" else "<empty>") 
+            attack_enum=ATTACK_TYPE.get_attack_method(scelta) 
+            if is_enum_member(attack_enum,ATTACK_TYPE): 
+>>>>>>> d122fbe (modified classes)
                 break
             msg="\n\nNessuna funzione trovata. Si vuole continuare? S/N\t" 
             if not ask_bool_choice(msg): 
@@ -29,6 +45,7 @@ class GET_ATTACK_FUNCTION:
         #Data in input una qualsiasi variabile ritorna l'enum associato quando possibile
         if is_enum_member(attack, ATTACK_TYPE): 
             return attack  
+<<<<<<< HEAD
         if is_enum_member(attack,Enum): 
             for function in (
                 lambda: ATTACK_TYPE[attack.name],
@@ -87,6 +104,35 @@ class GET_ATTACK_FUNCTION:
         except Exception as e: 
             print(e)
         
+=======
+        elif is_enum_member(attack,Enum): 
+            try: 
+                return ATTACK_TYPE[attack.name] 
+            except KeyError as k: 
+                print("STRINGA NON VALIDA",k)
+            try: 
+                return ATTACK_TYPE(attack.value)
+            except ValueError as v: 
+                print("INTEGER NON VALIDO",v) 
+        elif is_integer(attack): 
+            try:
+                return ATTACK_TYPE(attack)
+            except ValueError as v:
+                print("INTEGER NON VALIDO",v)
+        elif is_string(attack): 
+            try:
+                return ATTACK_TYPE(int(attack))
+            except ValueError as k: 
+                print("STRINGA NON VALIDA",k)
+            try:
+                return ATTACK_TYPE[attack]
+            except KeyError as k: 
+                print("STRINGA NON VALIDA",k)
+        
+        return None
+    
+    def get_description(attack:Enum=None)->str: 
+>>>>>>> d122fbe (modified classes)
         if is_enum_member(attack, ATTACK_TYPE): 
             match attack: 
                 case ATTACK_TYPE.ipv4_destination_unreachable: 
@@ -140,6 +186,7 @@ class GET_ATTACK_FUNCTION:
         raise Exception("Attacco immesso non valido: ",attack) 
     
     def print_available_attack(): 
+<<<<<<< HEAD
         print_enum(ATTACK_TYPE)
         time.sleep(1)
         print("\n Per scegliere un attacco, usa il nome o il numero corrispondente.") 
@@ -150,3 +197,19 @@ class GET_ATTACK_FUNCTION:
             \n\t*Numero '0'.
         """) 
         time.sleep(1.5) 
+=======
+        time.sleep(0.5)
+        print("Gli attacchi disponibili sono:\n")
+        for enumerator in list(ATTACK_TYPE): 
+            time.sleep(2) 
+            print(f" *{enumerator.name}:{enumerator.value}\n\t{ATTACK_TYPE.get_description(enumerator)}\n") 
+        time.sleep(0.5) 
+        print("\n Per scegliere un attacco, usa il nome o il numero corrispondente.") 
+        time.sleep(1) 
+        print(""" 
+            \nAd esempio per l'attacco ICMPv4 Destination Unreachable, puoi scegliere: 
+            \n\t*Il nome 'ipv4_destination_unreachable' 
+            \n\t\toppure'.
+            \n\t*Il numero '0'.
+        """) 
+>>>>>>> d122fbe (modified classes)
