25cc9b96698ce57e8540ba96a80de874cbe20d89
Marco Mecarelli <61580913+mcrmrc@users.noreply.github.com>
2026-07-15 21:11:50 +0200

    file comunication_methods - file finale della libreria

--- implementazione/unione/comunication_methods.py
+++ implementazione/unione/comunication_methods.py
@@ -1,3 +1,4 @@
+from scapy.all import IP, ICMP, Raw, sr1, AsyncSniffer
 from scapy.all import *
 import threading 
 import argparse
@@ -5,26 +6,33 @@ import mymethods
 import time
 import re
 
-ip_reg_pattern=r"\d+\.\d+\.\d+\.\d+" 
-pkt_conn_arrived = threading.Event()
-pkt_received_event = threading.Event()
+CONNECT="__CONNECT__"
+CONFIRM_ATTACKER="__CONFIRM_ATTACKER__"
+CONFIRM_VICTIM="__CONFIRM_VICTIM__"
+CONFIRM_PROXY="CONFIRM_PROXY"
 
-def send_packet(data="",ip_dst=None, time=10):
+ip_reg_pattern=r"^\d+\.\d+\.\d+\.\d+$"
+
+pkt_conn_received = threading.Event() 
+def wait_pkt_conn_received():
+    pkt_conn_received.wait() 
+    pkt_conn_received.clear()
+def set_pkt_conn_received():
+    pkt_conn_received.set()
+
+def send_packet(data:bytes=None,ip_dst=None, time=10):
     if type(ip_dst) is not str or re.match(ip_reg_pattern, ip_dst) is None :
         raise Exception("IP non valido")
-    if type(data) is not str or data is None:
-        raise Exception("Dati non validi") 
+    if data is None or type(data) is not bytes: 
+        print(data)
+        print(type(data))
+        raise Exception("I dati devono essere bytes")  
     
-    data="" if data is None else str(data)
-    icmp_id=mymethods.calc_checksum(data.encode())
-    gateway=".".join(
-        ip_dst.split(".")[index] if index!=3 else "0" 
-        for index in range(len(ip_dst.split(".")))
-    )
+    icmp_id=mymethods.calc_checksum(data) 
 
-    pkt = IP(dst=ip_dst)/ICMP(id=icmp_id) / data.encode() 
+    pkt = IP(dst=ip_dst)/ICMP(id=icmp_id) / data  
     print(f"Sending...")
-    print(f"{pkt.show()}")
+    print(f"{pkt.summary()}") #print(f"{pkt.show()}")
     ans = sr1(pkt, timeout=time, verbose=1)
     if ans:
         print(f"Reply: \t{ip_dst} is alive\n")
@@ -35,25 +43,23 @@ def send_packet(data="",ip_dst=None, time=10):
 
 def sniffer_timeout():
     global sniffer
-    if not pkt_received_event.is_set():
+    if not pkt_conn_received.is_set():
         print("Timeout: No packet received within 60 seconds")
         sniffer.stop() 
-        pkt_conn_arrived.set()
+        set_pkt_conn_received()
 
-def proxy_callback(packet):
+def nonusable_callback(packet):
     global ip_attacker, proxy_ip, timeout_timer
     print("comunication_callback")
     print("Packet received: \n{}".format(packet.summary()))     
     if packet.haslayer(IP) and packet.haslayer(ICMP) and packet.haslayer(Raw):
-        if b'__CONNECT__' in bytes(packet[Raw].load):
+        if CONNECT.encode() in packet[Raw].load:
             print(f"{packet[IP].src} si vuole collegare") 
             #proxy_ip.append(packet[IP].src) if packet[IP].src not in proxy_ip else None
-            #num_proxy = packet[Raw].load.replace("__CONNECT__","")
+            #num_proxy = packet[Raw].load.replace(CONNECT,"")
             print("Mittente: \t{}".format(packet[IP].src))
             print("Payload: \t{}".format(bytes(packet[Raw].load)))  
-            pkt_received_event.set()
-            timeout_timer.cancel()
-            pkt_conn_arrived.set() 
+            set_pkt_conn_received()
 
 def sniff_packet(args:dict,timeout_time=60):
     global sniffer, gateway,timeout_timer
@@ -64,6 +70,9 @@ def sniff_packet(args:dict,timeout_time=60):
     ] 
     if args is None or type(args) is not dict:
         raise Exception("Argomenti non validi") 
+    if timeout_time is not None and type(int(timeout_time)) is not int: 
+        raise Exception("Tempo non accettato") 
+    timeout_time=int(timeout_time) if timeout_time is not None else timeout_time
     invalid_args=[key for key in args if key not in accepted_key_dict]
     if len(invalid_args):
         raise ValueError(f"Invalid keys in dictionary {invalid_args}")
@@ -93,13 +102,17 @@ def check_connessione(ip_dst,args=None):
     
     print(f"Controllo la connessione con {ip_dst}...")
     sniff_packet(args) 
-    pkt_conn_arrived.wait()
+    wait_pkt_conn_received()
     if sniffer.running:
         sniffer.stop()
         sniffer.join()
-    data=b"__CONNECT__ "
-    if send_packet(data,ip_dst):
-        return True
+    if timeout_timer.is_alive():
+        timeout_timer.cancel()
+        data=CONNECT.encode()
+        if send_packet(data,ip_dst):
+            print(f"Stabilita connessione con {ip_dst}")
+            return True
+    print(f"Nessuna connessione con {ip_dst}")
     return False
 
 def test_connection(ip_dst,args:dict,data=None,timeout_time=60):
@@ -109,12 +122,12 @@ def test_connection(ip_dst,args:dict,data=None,timeout_time=60):
     print(f"Tento la connessione con {ip_dst}...")
     if type(data) is not str or data is None:
         raise Exception("Dati non validi") 
-    #data="".join( "__CONNECT__ {}".format(ip_vittima))
+    #data="".join(f"{CONNECT} {ip_vittima}")
     if send_packet(data,ip_dst): 
         if args is None or type(args) is not dict:
             raise Exception("Argomenti non validi")
         sniff_packet(args) 
-        pkt_conn_arrived.wait()
+        wait_pkt_conn_received()
         if sniffer.running:
             sniffer.stop()
             sniffer.join()
@@ -130,11 +143,11 @@ if __name__=="__main__":
     args_dict={
         "filter":f"icmp and src {ip_dst}"
         #,"count":1 
-        ,"prn":proxy_callback 
+        ,"prn":nonusable_callback 
         #,"store":True 
         ,"iface":mymethods.iface_from_IP(ip_dst)[1]
     }
-    data="__CONNECT__ 192.168.56.102"
+    data=f"{CONNECT} 192.168.56.102"
     test_connection(ip_dst,args_dict,data)
     exit(0)
     args=set_args_parser()
@@ -156,13 +169,12 @@ if __name__=="__main__":
     args_sniff={
         "filter": f"icmp and dst {ip_host}" 
         #,"count":1 
-        ,"prn":proxy_callback 
+        ,"prn":nonusable_callback 
         #,store:True 
         ,"iface":mymethods.iface_from_IP(gateway)[1]  
     }
     sniff_packet(args_sniff)
-
-    pkt_conn_arrived.wait() 
+    wait_pkt_conn_received() 
     if sniffer.running:
         sniffer.stop()
         sniffer.join() 
@@ -174,6 +186,6 @@ if __name__=="__main__":
         #,"192.168.56.xxx"
     ]  
     for proxy in ip_proxy: 
-        send_packet(data="__CONNECT__", ip_dst=proxy)
+        send_packet(data=CONNECT.encode(), ip_dst=proxy)
 
 
