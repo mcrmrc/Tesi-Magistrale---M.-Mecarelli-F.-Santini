9a0d74b5d01b2c003af3596aba6255fe0676ec20
Marco Mecarelli <61580913+mcrmrc@users.noreply.github.com>
2026-07-15 21:11:58 +0200

    modificato file test_script

--- implementazione/unione/scelta_type/testscript.py
+++ implementazione/unione/scelta_type/testscript.py
@@ -7,12 +7,7 @@ import os
 import argparse  
 import threading 
 import json 
-import socket
-
-file_path = "../comunication_methods.py"
-directory = os.path.dirname(file_path)
-sys.path.insert(0, directory)
-import comunication_methods as com
+import socket 
 
 file_path = "../mymethods.py"
 directory = os.path.dirname(file_path)
@@ -24,17 +19,34 @@ directory = os.path.dirname(file_path)
 sys.path.insert(0, directory)
 import attacksingleton 
 
-attack_function=attacksingleton.AttackType().get_attack_function("ipv4_1")
-data="ls -l"
-ip_dst=ipaddress.ip_address("192.168.1.20")
-interface,_=mymethods.iface_src_from_IP(ip_dst)
-print("interface: ",interface)
+data="echo 'Ciao'".encode()
 
-pkt=IP(dst="192.168.1.20")/ICMP() 
-print(pkt.summary())
-print(pkt.show())
-ans=sr1(pkt, verbose=1, timeout=3, iface=interface)
-if ans:
-    print(ans.summary())
+print(f"Data Byte: {data}")
 
-#attacksingleton.send_data(attack_function, data.encode(), ip_dst)
+bit_data=[]
+for piece_data in data: #BIG ENDIAN
+    bit_data.append([(piece_data >> index) & 1 for index in range(8)]) #LSB 
+print(f"Bin extracetd: {bit_data}") 
+
+print("Bin normali")
+data="echo 'Ciao'".encode()
+for byte in data: 
+    #print(f"Byte: {bin(byte)}")
+    for bit in bin(byte): 
+        print(f"{bit}",end=" ")
+    print()
+
+timing_data=[]
+for bit in bit_data:
+    timing_data.append(bit)    
+print(f"Bit ricevuti: {timing_data}") 
+
+str_data=""
+for integer in timing_data:
+    str_data+=format(integer, f'0{2}b') 
+raw_data="" 
+for index in range(0, len(str_data), 8):
+    int_data=0
+    for bit in str_data[index:index+8][::-1]:
+        int_data=int_data<<1|int(bit)
+    raw_data+=chr(int_data) 
